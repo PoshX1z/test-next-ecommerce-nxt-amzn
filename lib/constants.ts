55e7ba8fdc14350fdd492ba3389260e4665173f2
PoshX1z <xdante1357@gmail.com>
2025-03-13 21:02:44 +0700

    Finish lesson 3

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+//Bring from .env.local and export as these constants
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "NxtAmzn";
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more.";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "An Amazon clone built with Next.js and MongoDB";
